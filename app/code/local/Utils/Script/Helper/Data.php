942fcb06ea1ce03690be4279b2db17adb6063b2b
iacopop <iacopop@localhost>
2014-03-25 10:48:44 +0100

    modulo utils

--- app/code/local/Utils/Script/Helper/Data.php
+++ app/code/local/Utils/Script/Helper/Data.php
@@ -1,36 +1,46 @@
 <?php
 
 class Utils_Script_Helper_Data extends Mage_Core_Helper_Abstract {
-    
-    public function test(){
+
+    public function test() {
         Mage::log("test helper");
     }
 
-    public function sendMail($subject, $message = "", $mailFrom, $fromName, $mailTo, $fullfilepath, $filename) {
-
-        $mail = new Zend_Mail('utf-8');
-
-        $recipients = array(
-            $mailTo
-        );
-
-        $mail->setBodyHtml($message)
-                ->setSubject($subject)
-                ->addTo($recipients)
-                ->setFrom($mailFrom, $fromName);
-
-        //file content is attached
-        $file = $fullfilepath;
-        $attachment = file_get_contents($file);
-        $mail->createAttachment(
-                $attachment, Zend_Mime::TYPE_OCTETSTREAM, Zend_Mime::DISPOSITION_ATTACHMENT, Zend_Mime::ENCODING_BASE64, $filename
-        );
-
-
+    public function sendTransactionalEmail($subject, $message, $mailFrom, $fromName, $mailTo, $fullfilepath, $filename) {
         try {
-            $mail->send();
+            // Transactional Email Template's ID
+            $templateId = 1;
+
+            // Set sender information          
+            $senderName = $fromName;
+            $senderEmail = $mailFrom;
+            $sender = array('name' => $senderName,
+                'email' => $senderEmail);
+
+            // Set recepient information
+            $recepientEmail = $mailTo;
+            $recepientName = 'Sun68';
+
+            // Get Store ID    
+            $storeId = Mage::app()->getStore()->getId();
+
+            // Set variables that can be used in email template
+            $vars = array();
+
+            $translate = Mage::getSingleton('core/translate');
+
+            // Send Transactional Email
+            $transactionalEmail = Mage::getModel('core/email_template')->setDesignConfig(array('area' => 'frontend', 'store' => 0));
+
+            $fileContents = file_get_contents($fullfilepath);
+
+            $transactionalEmail->getMail()->createAttachment($fileContents, 'text/csv')->filename = $filename;
+
+            $transactionalEmail->sendTransactional($templateId, $sender, $recepientEmail, $recepientName, $vars, $storeId);
+
+            $translate->setTranslateInline(true);
         } catch (Exception $e) {
-            Mage::logException($e);
+            Mage::log($e->getMessage(), null, 'exporter.log');
         }
     }
 
